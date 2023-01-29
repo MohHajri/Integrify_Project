8f14cbf1f32ea3be98d7d96cc2063bca804c672a
hajri <c1almo00@students.oamk.fi>
2023-01-29 22:12:39 +0200

    another api used, all info displayed, search feature and other nice stuff coming!

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,67 +1,81 @@
-import React, { useEffect } from 'react'
-import Navbar from '../components/Navbar'
-import { useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Navbar from '../components/Navbar';
 
-function Home() {
-    const [countries, setCountries] = useState([]);
-    // const [search, setSearch] = useState("");
-
-    const getCountries = async () => {
-      fetch('https://restcountries.com/v3.1/all')
-  .then(response => response.json())
-          .then(data => {
-               const newData = data.map(({ name, region, population, languages, flag }) => ({
-                  name: name.common,
-                  region,
-                  population,
-                //   languages: languages,
-                  flags: flag.png,
-              }));
-               setCountries(newData);
-          }    
-              
-        );
-    };
-     console.log(countries);
+const Home = () => {
+  const [countries, setCountries] = useState([]);
+  const [loading, setLoading] = useState(true);
+  
 
     useEffect(() => {
-        getCountries();
-    }, []);
+    const fetchData = async () => {
+      try {
+        const [flagResp, capResp, popResp] = await Promise.all([
+          axios.get('https://countriesnow.space/api/v0.1/countries/flag/images'),
+          axios.get('https://countriesnow.space/api/v0.1/countries/capital'),
+          axios.get('https://countriesnow.space/api/v0.1/countries/population'),
+        ]);
 
+        const flagData = flagResp.data.data;
+        const capData = capResp.data.data;
+        const popData = popResp.data.data;
 
-    return (
-      <div className="home-page">
-          <Navbar />
-            <div className="body">
-              <table className='table'>
-                    <thead className="thead-dark">
-                  <tr>
-                      <th  scope="col">Flag</th>
-                      <th  scope="col">Name</th>
-                      <th  scope="col">Region</th>
-                      <th  scope="col">Population</th>
-                      {/* <th  scope="col">Languages</th> */}
-                      
-                  </tr>
-              </thead>
-                <tbody>
-               {countries.map((country) => (
-                                    <tr key={country.cca3}>
-                                <td>
-                                    <img src={country.flag} alt="" />
-                                </td>
-                                    <td>{country.name}</td>
-                                    <td>{country.region}</td>
-                                    <td>{country.population}</td>
-                                    </tr>
-                                
-                            ))}
-                    </tbody>
-              </table>
-              
-          </div>
-      </div>
-  )
-}
+        const newData = flagData.map(flag => {
+          const cap = capData.find(c => c.name === flag.name);
+          const pop = popData.find(p => p.code === flag.iso3);
+          const latestPop =  pop && pop.populationCounts
+          ? pop.populationCounts.sort((a, b) => b.year - a.year)[0].value
+          : null;
 
-export default Home
+          return {
+            flag: flag.flag,
+            name: flag.name,
+            capital: cap.capital,
+            population: latestPop,
+          };
+        });
+
+        setCountries(newData);
+        setLoading(false);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="home-page">
+      <Navbar />
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <table className="table">
+          <thead className="thead-dark">
+            <tr>
+              <th>Flag</th>
+              <th>Name</th>
+              <th>Capital</th>
+              <th>Population</th>
+            </tr>
+          </thead>
+          <tbody>
+            {countries.map(country => (
+              <tr key={country.name}>
+                <td>
+                  <img src={country.flag} alt={country.name} />
+                </td>
+                <td>{country.name}</td>
+                <td>{country.capital}</td>
+                <td>{country.population}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+};
+
+export default Home;
